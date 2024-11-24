e4a9e5482b3fbbe3a768a8718fc157a7497c62d4
tranthien079 <106481539+tranthien079@users.noreply.github.com>
2024-11-24 23:11:55 +0700

    Update ReceiptPage.jsx

--- admin/src/pages/admin/ReceiptPage.jsx
+++ admin/src/pages/admin/ReceiptPage.jsx
@@ -1,11 +1,13 @@
-import React from 'react'
-import { Space, Table, Tag, Popconfirm, Tooltip, Button, Typography, Image } from 'antd';
+import React, { useRef, useState } from 'react'
+import { Space, Table, Tag, Popconfirm, Tooltip, Button, Typography, Image, Input, DatePicker } from 'antd';
 import { useDispatch, useSelector } from "react-redux";
 import { useEffect } from 'react';
 import { getReceipt, deleteReceipt } from '../../redux/receipt/receiptSlice';
-import { EditOutlined, DeleteOutlined } from '@ant-design/icons'
+import { EditOutlined, DeleteOutlined, SearchOutlined } from '@ant-design/icons'
 import { Link } from 'react-router-dom';
 import { toast } from 'react-toastify';
+import Highlighter from 'react-highlight-words';
+import dayjs from 'dayjs';
 
 const ReceiptPage = () => {
   const dispatch = useDispatch();
@@ -25,6 +27,160 @@ const ReceiptPage = () => {
       console.log(error)
     }
   };
+
+     //search table for keywords
+     const [searchText, setSearchText] = useState('');
+     const [searchedColumn, setSearchedColumn] = useState('');
+     const searchInput = useRef(null);
+  
+     const handleSearch = (selectedKeys, confirm, dataIndex) => {
+       confirm();
+       setSearchText(selectedKeys[0]);
+       setSearchedColumn(dataIndex);
+     };
+   
+     const handleReset = (clearFilters) => {
+       clearFilters();
+       setSearchText('');
+     };
+   
+     const getColumnSearchProps = (dataIndex) => ({
+      filterDropdown: ({ setSelectedKeys, selectedKeys, confirm, clearFilters }) => (
+        <div
+          style={{
+            padding: 8,
+          }}
+          onKeyDown={(e) => e.stopPropagation()}
+        >
+          <Input
+            ref={searchInput}
+            placeholder={`Search ${dataIndex}`}
+            value={selectedKeys[0]}
+            onChange={(e) => setSelectedKeys(e.target.value ? [e.target.value] : [])}
+            onPressEnter={() => handleSearch(selectedKeys, confirm, dataIndex)}
+            style={{
+              marginBottom: 8,
+              display: 'block',
+            }}
+          />
+          <Space>
+            <Button
+              type="primary"
+              onClick={() => handleSearch(selectedKeys, confirm, dataIndex)}
+              icon={<SearchOutlined />}
+              size="small"
+              style={{
+                width: 90,
+              }}
+            >
+              Search
+            </Button>
+            <Button
+              onClick={() => clearFilters && handleReset(clearFilters)}
+              size="small"
+              style={{
+                width: 90,
+              }}
+            >
+              Reset
+            </Button>
+          </Space>
+        </div>
+      ),
+      filterIcon: (filtered) => (
+        <SearchOutlined
+          style={{
+            color: filtered ? '#1677ff' : undefined,
+          }}
+        />
+      ),
+      onFilter: (value, record) =>
+        record[dataIndex].toString().toLowerCase().includes(value.toLowerCase()),
+      onFilterDropdownOpenChange: (visible) => {
+        if (visible) {
+          setTimeout(() => searchInput.current?.select(), 100);
+        }
+      },
+      render: (text) =>
+        searchedColumn === dataIndex ? (
+          <Highlighter
+            highlightStyle={{
+              backgroundColor: '#ffc069',
+              padding: 0,
+            }}
+            searchWords={[searchText]}
+            autoEscape
+            textToHighlight={text ? text.toString() : ''}
+          />
+        ) : (
+          text
+        ),
+    });
+
+    const getColumnSearchPropsDate = (dataIndex) => {
+      return {
+        filterDropdown: ({
+          setSelectedKeys,
+          selectedKeys,
+          confirm,
+          clearFilters,
+        }) => (
+          <div style={{ padding: 8 }}>
+            <DatePicker.RangePicker
+              style={{ width: "100%", marginBottom: 8 }}
+              value={
+                selectedKeys[0]
+                  ? [dayjs(selectedKeys[0][0]), dayjs(selectedKeys[0][1])]
+                  : null
+              }
+              onChange={(dates) => {
+                if (dates) {
+                  setSelectedKeys([
+                    [
+                      dates[0].startOf("day").toISOString(),
+                      dates[1].endOf("day").toISOString(),
+                    ],
+                  ]);
+                } else {
+                  setSelectedKeys([]);
+                }
+              }}
+            />
+            <div style={{ display: "flex", justifyContent: "space-between" }}>
+              <button
+                type="button"
+                onClick={() => {
+                  clearFilters();
+                  confirm();
+                }}
+                style={{ width: 90 }}
+              >
+                Reset
+              </button>
+              <button
+                type="button"
+                onClick={() => confirm()}
+                style={{ width: 90 }}
+              >
+                Ok
+              </button>
+            </div>
+          </div>
+        ),
+        onFilter: (value, record) => {
+          if (!value || !record[dataIndex]) return false;
+  
+          const recordDate = dayjs(record[dataIndex]);
+          const [startDate, endDate] = value;
+  
+          return recordDate.isAfter(startDate) && recordDate.isBefore(endDate);
+        },
+        filterIcon: (filtered) => (
+          <span style={{ color: filtered ? "#1890ff" : undefined }}>🗓</span>
+        ),
+      };
+    };
+
   const columns = [
     {
       title: 'Stt',
@@ -40,11 +196,13 @@ const ReceiptPage = () => {
       key: 'userId',
       // align: 'center',
       render: (text) => <a>{text}</a>,
+      ...getColumnSearchProps('userId')
     },
     {
       title: 'Nhà cung cấp',
       dataIndex: 'supplierId',
       key: 'supplierId',
+      ...getColumnSearchProps('supplierId')
     },
     {
       title: 'Tổng tiền',
@@ -59,6 +217,7 @@ const ReceiptPage = () => {
         key: 'importDate',
         render: (importDate) => <span>{new Date(importDate).toLocaleDateString()}</span>,
         // align: 'center',
+        ...getColumnSearchPropsDate('importDate')
       },
     {
       title: "Action",
