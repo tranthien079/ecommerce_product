165afa73f72c15c768a3c2053b604b18a60fc830
tranthien079 <106481539+tranthien079@users.noreply.github.com>
2024-11-24 23:10:14 +0700

    Update BlogPage.jsx

--- admin/src/pages/admin/BlogPage.jsx
+++ admin/src/pages/admin/BlogPage.jsx
@@ -1,11 +1,12 @@
-import React from 'react'
-import { Space, Table, Popconfirm, Tooltip, Button, Image } from 'antd';
+import React, { useRef, useState } from 'react'
+import { Space, Table, Popconfirm, Tooltip, Button, Image, Input } from 'antd';
 import { useDispatch, useSelector } from "react-redux";
 import { useEffect } from 'react';
 import { getBlog, deleteBlog } from '../../redux/blog/blogSlice';
-import { EditOutlined, DeleteOutlined } from '@ant-design/icons'
+import { EditOutlined, DeleteOutlined, SearchOutlined } from '@ant-design/icons'
 import { Link } from 'react-router-dom';
 import { toast } from 'react-toastify';
+import Highlighter from 'react-highlight-words';
 
 const BlogPage = () => {
   const dispatch = useDispatch();
@@ -27,6 +28,95 @@ const BlogPage = () => {
       });
   };
 
+   //search table for keywords
+   const [searchText, setSearchText] = useState('');
+   const [searchedColumn, setSearchedColumn] = useState('');
+   const searchInput = useRef(null);
+
+   const handleSearch = (selectedKeys, confirm, dataIndex) => {
+     confirm();
+     setSearchText(selectedKeys[0]);
+     setSearchedColumn(dataIndex);
+   };
+ 
+   const handleReset = (clearFilters) => {
+     clearFilters();
+     setSearchText('');
+   };
+ 
+   const getColumnSearchProps = (dataIndex) => ({
+    filterDropdown: ({ setSelectedKeys, selectedKeys, confirm, clearFilters }) => (
+      <div
+        style={{
+          padding: 8,
+        }}
+        onKeyDown={(e) => e.stopPropagation()}
+      >
+        <Input
+          ref={searchInput}
+          placeholder={`Search ${dataIndex}`}
+          value={selectedKeys[0]}
+          onChange={(e) => setSelectedKeys(e.target.value ? [e.target.value] : [])}
+          onPressEnter={() => handleSearch(selectedKeys, confirm, dataIndex)}
+          style={{
+            marginBottom: 8,
+            display: 'block',
+          }}
+        />
+        <Space>
+          <Button
+            type="primary"
+            onClick={() => handleSearch(selectedKeys, confirm, dataIndex)}
+            icon={<SearchOutlined />}
+            size="small"
+            style={{
+              width: 90,
+            }}
+          >
+            Search
+          </Button>
+          <Button
+            onClick={() => clearFilters && handleReset(clearFilters)}
+            size="small"
+            style={{
+              width: 90,
+            }}
+          >
+            Reset
+          </Button>
+        </Space>
+      </div>
+    ),
+    filterIcon: (filtered) => (
+      <SearchOutlined
+        style={{
+          color: filtered ? '#1677ff' : undefined,
+        }}
+      />
+    ),
+    onFilter: (value, record) =>
+      record[dataIndex].toString().toLowerCase().includes(value.toLowerCase()),
+    onFilterDropdownOpenChange: (visible) => {
+      if (visible) {
+        setTimeout(() => searchInput.current?.select(), 100);
+      }
+    },
+    render: (text) =>
+      searchedColumn === dataIndex ? (
+        <Highlighter
+          highlightStyle={{
+            backgroundColor: '#ffc069',
+            padding: 0,
+          }}
+          searchWords={[searchText]}
+          autoEscape
+          textToHighlight={text ? text.toString() : ''}
+        />
+      ) : (
+        text
+      ),
+  });
+
   const columns = [
     {
       title: 'Tiêu đề',
@@ -43,6 +133,7 @@ const BlogPage = () => {
       title: 'Tác giả',
       dataIndex: 'author_name',
       key: 'author_name',
+      ...getColumnSearchProps('author_name'),
     },
     {
       title: 'Hình ảnh',
