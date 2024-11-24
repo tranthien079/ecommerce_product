1b733ce26c8db7f99ca87c14ec5e7febe93f0943
tranthien079 <106481539+tranthien079@users.noreply.github.com>
2024-11-24 23:12:19 +0700

    Update SupplierPage.jsx

--- admin/src/pages/admin/SupplierPage.jsx
+++ admin/src/pages/admin/SupplierPage.jsx
@@ -1,11 +1,12 @@
-import React from 'react'
-import { Space, Table, Tag, Popconfirm, Tooltip, Button, Typography, Image } from 'antd';
+import React, { useRef, useState } from 'react'
+import { Space, Table, Tag, Popconfirm, Tooltip, Button, Typography, Image, Input } from 'antd';
 import { useDispatch, useSelector } from "react-redux";
 import { useEffect } from 'react';
 import { getSupplier, deleteSupplier } from '../../redux/supplier/supplierSlice';
-import { EditOutlined, DeleteOutlined } from '@ant-design/icons'
+import { EditOutlined, DeleteOutlined, SearchOutlined } from '@ant-design/icons'
 import { Link } from 'react-router-dom';
 import { toast } from 'react-toastify';
+import Highlighter from 'react-highlight-words';
 
 const SupplierPage = () => {
   const dispatch = useDispatch();
@@ -25,31 +26,127 @@ const SupplierPage = () => {
       toast.error('Có lỗi xảy ra khi xóa nhà cung cấp');
     }
   };
+
+     //search table for keywords
+     const [searchText, setSearchText] = useState('');
+     const [searchedColumn, setSearchedColumn] = useState('');
+     const searchInput = useRef(null);
+  
+     const handleSearch = (selectedKeys, confirm, dataIndex) => {
+       confirm();
+       setSearchText(selectedKeys[0]);
+       setSearchedColumn(dataIndex);
+     };
+   
+     const handleReset = (clearFilters) => {
+       clearFilters();
+       setSearchText('');
+     };
+   
+     const getColumnSearchProps = (dataIndex) => ({
+      filterDropdown: ({ setSelectedKeys, selectedKeys, confirm, clearFilters }) => (
+        <div
+          style={{
+            padding: 8,
+          }}
+          onKeyDown={(e) => e.stopPropagation()}
+        >
+          <Input
+            ref={searchInput}
+            placeholder={`Search ${dataIndex}`}
+            value={selectedKeys[0]}
+            onChange={(e) => setSelectedKeys(e.target.value ? [e.target.value] : [])}
+            onPressEnter={() => handleSearch(selectedKeys, confirm, dataIndex)}
+            style={{
+              marginBottom: 8,
+              display: 'block',
+            }}
+          />
+          <Space>
+            <Button
+              type="primary"
+              onClick={() => handleSearch(selectedKeys, confirm, dataIndex)}
+              icon={<SearchOutlined />}
+              size="small"
+              style={{
+                width: 90,
+              }}
+            >
+              Search
+            </Button>
+            <Button
+              onClick={() => clearFilters && handleReset(clearFilters)}
+              size="small"
+              style={{
+                width: 90,
+              }}
+            >
+              Reset
+            </Button>
+          </Space>
+        </div>
+      ),
+      filterIcon: (filtered) => (
+        <SearchOutlined
+          style={{
+            color: filtered ? '#1677ff' : undefined,
+          }}
+        />
+      ),
+      onFilter: (value, record) =>
+        record[dataIndex].toString().toLowerCase().includes(value.toLowerCase()),
+      onFilterDropdownOpenChange: (visible) => {
+        if (visible) {
+          setTimeout(() => searchInput.current?.select(), 100);
+        }
+      },
+      render: (text) =>
+        searchedColumn === dataIndex ? (
+          <Highlighter
+            highlightStyle={{
+              backgroundColor: '#ffc069',
+              padding: 0,
+            }}
+            searchWords={[searchText]}
+            autoEscape
+            textToHighlight={text ? text.toString() : ''}
+          />
+        ) : (
+          text
+        ),
+    });
   const columns = [
 
     {
-      title: 'Name',
+      title: 'Tên nhà cung cấp',
       dataIndex: 'name',
       key: 'name',
       // align: 'center',
       render: (text) => <a>{text}</a>,
+      ...getColumnSearchProps('name')
     },
     {
       title: 'Email',
       dataIndex: 'email',
       key: 'email',
+      ...getColumnSearchProps('email')
+
     },
     {
       title: 'Số điện thoại',
       dataIndex: 'mobile',
       key: 'mobile',
       // align: 'center',
+      ...getColumnSearchProps('mobile')
+
     },
     {
       title: 'Địa chỉ',
       dataIndex: 'address',
       key: 'address',
       // align: 'center',
+      ...getColumnSearchProps('address')
+
     },
     
     {
@@ -90,4 +187,4 @@ const SupplierPage = () => {
     )
 }
 
-export default SupplierPage
+export default SupplierPage;
