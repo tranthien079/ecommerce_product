79799e836fa02490cff9e58ccf0c37197b123ff0
tranthien079 <106481539+tranthien079@users.noreply.github.com>
2024-11-30 18:18:14 +0700

    Update receiptRoute.js

--- server/routes/receiptRoute.js
+++ server/routes/receiptRoute.js
@@ -5,12 +5,14 @@ const {
   deleteReceipt,
   getReceiptById,
   getAllReceipts,
+  cancelReceipt
 } = require("../controllers/receiptController");
 const { authMiddleWare, isAdmin } = require('../middlewares/authMiddleware');
 const router = express.Router();
 
 router.post("/create", authMiddleWare, isAdmin, createReceipt);
-router.put("/:id", authMiddleWare, isAdmin, updateReceipt);
+// router.put("/:id", authMiddleWare, isAdmin, updateReceipt);
+router.put("/:id", authMiddleWare, isAdmin, cancelReceipt);
 router.delete("/:id", authMiddleWare, isAdmin, deleteReceipt);
 router.get("/:id", getReceiptById);
 router.get("/", getAllReceipts);
