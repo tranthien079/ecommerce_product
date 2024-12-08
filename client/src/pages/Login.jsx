c7c543593a3544da11745bfb0a743777de7ea977
tranthien079 <106481539+tranthien079@users.noreply.github.com>
2024-12-08 18:53:46 +0700

    Update Login.jsx

--- client/src/pages/Login.jsx
+++ client/src/pages/Login.jsx
@@ -92,7 +92,10 @@ const Login = () => {
             Quên mật khẩu?
           </Link>
         </div>
-        <button className="bg-black  text-white font-light px-8 py-2 mt-4 w-full">
+        <button className="px-6 py-2 bg-black text-white rounded-md hover:bg-gray-700 
+                               focus:outline-none focus:ring-2 focus:ring-gray-500 focus:ring-offset-2 
+                               transition-colors disabled:opacity-50 disabled:cursor-not-allowed
+                               flex items-center space-x-2 w-full justify-center">
           Đăng nhập
         </button>
         <Typography color="gray" className="mt-4 text-center font-normal">
