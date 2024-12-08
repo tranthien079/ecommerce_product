b6385d66867a0c4bfff6dca983fefec4e2022cc7
tranthien079 <106481539+tranthien079@users.noreply.github.com>
2024-12-08 19:03:29 +0700

    Update Login.jsx

--- client/src/pages/Login.jsx
+++ client/src/pages/Login.jsx
@@ -92,7 +92,7 @@ const Login = () => {
             Quên mật khẩu?
           </Link>
         </div>
-        <button className="px-6 py-2 bg-black text-white rounded-md hover:bg-gray-700 
+        <button className="px-6 py-2 bg-black text-white rounded-md hover:bg-gray-700  mt-2
                                focus:outline-none focus:ring-2 focus:ring-gray-500 focus:ring-offset-2 
                                transition-colors disabled:opacity-50 disabled:cursor-not-allowed
                                flex items-center space-x-2 w-full justify-center">
