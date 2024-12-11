00eefe61d144097834969b9dc6635b7627b7fd30
tranthien079 <106481539+tranthien079@users.noreply.github.com>
2024-12-11 12:03:14 +0700

    Update OurPolicy.jsx

--- client/src/components/OurPolicy.jsx
+++ client/src/components/OurPolicy.jsx
@@ -6,18 +6,18 @@ const OurPolicy = () => {
     <div className='flex flex-col sm:flex-row justify-around gap-12 sm:gap-2 text-center py-20 text-xs sm:text-sm md:text-base text-gray-700'>
       <div>
         <img src={assets.exchange_icon} className='w-12 m-auto mb-5' alt="" />
-        <p className=' font-semibold'>Easy Exchange Policy</p>
-        <p className=' text-gray-400'>We offer hassle free exchange policy</p>
+        <p className=' font-semibold'>Chính sách đổi trả dễ dàng</p>
+        <p className=' text-gray-400'>Chúng tôi cung cấp chính sách đổi trả nhanh chóng</p>
       </div>
       <div>
         <img src={assets.quality_icon} className='w-12 m-auto mb-5' alt="" />
-        <p className=' font-semibold'>7 Days Return Policy</p>
-        <p className=' text-gray-400'>We provide 7 days return free policy</p>
+        <p className=' font-semibold'>Chính sách thanh toán</p>
+        <p className=' text-gray-400'>Bảo mật thông tin thanh toán của khách hàng</p>
       </div>
       <div>
         <img src={assets.support_img} className='w-12 m-auto mb-5' alt="" />
-        <p className=' font-semibold'>Best customer support</p>
-        <p className=' text-gray-400'>We provide 24/7 customer support</p>
+        <p className=' font-semibold'>Hỗ trợ khách hàng</p>
+        <p className=' text-gray-400'>Chúng tôi cung cấp dịch vụ hỗ trợ 24/7</p>
       </div>
     </div>
   )
